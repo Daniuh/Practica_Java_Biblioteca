afff187bd0062295489849dd0b8c49cceac4ecbc
Daniel <naranjobenavidesd@gmail.com>
2024-03-25 17:35:38 -0500

    Se crea los metodos get y setter de la clase libro y los demas metodos a utilizar

---
+++ src/ManejoUsuario/Menu.java
@@ -0,0 +1,10 @@
+package ManejoUsuario;
+
+import Modelos.Libro;
+
+import java.util.ArrayList;
+
+public class Menu {
+    ArrayList<Libro> libro = new ArrayList<>();
+
+}
--- src/Modelos/Libro.java
+++ src/Modelos/Libro.java
@@ -1,7 +1,41 @@
 package Modelos;
 
+import java.util.ArrayList;
+
 public class Libro {
-    private String titulo;
-    private String autor;
-    private Integer ejemplaresDisponibles;
+
+    private String autor = "";
+    private String titulo = "";
+    private Integer ejemplaresDisponibles = 0;
+
+    public Libro() {
+    }
+
+    public String getAutor() {return autor;}
+
+    public void setAutor(String autor) {this.autor = autor;}
+
+    public String getTitulo() {return titulo;}
+
+    public void setTitulo(String titulo) {this.titulo = titulo;}
+
+    public Integer getEjemplaresDisponibles() {return ejemplaresDisponibles;}
+
+    public void setEjemplaresDisponibles(Integer ejemplaresDisponibles) {this.ejemplaresDisponibles = ejemplaresDisponibles;}
+
+    public void registrarLibro(){
+
+    }
+
+    public void prestarLibro(){
+
+    }
+
+    public void devolverLibro(){
+
+    }
+
+    public void mostrarInformacion(){
+
+    }
 }
